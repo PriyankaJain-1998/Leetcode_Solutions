be096a6ffc5de864ca5115a45880bbaa9940183a
PriyankaJain-1998 <58905207+PriyankaJain-1998@users.noreply.github.com>
2024-06-17 10:26:12 -0400

    Time: 115 ms (35.89%), Space: 16.5 MB (39.72%) - LeetHub

--- 0633-sum-of-square-numbers/0633-sum-of-square-numbers.py
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.py
@@ -1,7 +1,8 @@
 class Solution:
     def judgeSquareSum(self, c: int) -> bool:
         for a in range(int(sqrt(c))+1):
-            b = (c-(a**2))**0.5
+            # b = (c-(a**2))**0.5
+            b = sqrt(c-a*a)
             if b == int(b):return True
         return False
         
