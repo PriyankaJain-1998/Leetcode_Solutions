38b828d20f1fb7e8e8202d6aa744af47fbe332ce
PriyankaJain-1998 <58905207+PriyankaJain-1998@users.noreply.github.com>
2024-08-20 17:13:18 -0400

    Time: 45 ms (5.11%), Space: 16.5 MB (89.12%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        while len(s)>0:
+            l = len(s)
+            s = s.replace("()",'').replace("[]",'').replace("{}",'')
+            if len(s)==l: return False
+
+        return True
+        
