c74c9e91f0b413b0276a6f4101ccad70071b9293
PriyankaJain-1998 <58905207+PriyankaJain-1998@users.noreply.github.com>
2024-09-01 22:58:21 -0400

    Time: 43 ms (36.15%), Space: 17.7 MB (43.49%) - LeetHub

---
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
@@ -0,0 +1,14 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        def dfs(node, depth):
+            if not node: return depth
+            return max(dfs(node.left, depth+1), dfs(node.right, depth+1))
+            dfs(node.right)
+
+        return dfs(root, 0)
