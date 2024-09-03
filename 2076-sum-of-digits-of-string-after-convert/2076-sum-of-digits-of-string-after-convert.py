7a3697178a03489852fd9902e1bddf9b4ba7c2bc
PriyankaJain-1998 <58905207+PriyankaJain-1998@users.noreply.github.com>
2024-09-02 23:22:16 -0400

    Time: 51 ms (5.42%), Space: 16.8 MB (5.1%) - LeetHub

---
+++ 2076-sum-of-digits-of-string-after-convert/2076-sum-of-digits-of-string-after-convert.py
@@ -0,0 +1,19 @@
+class Solution:
+    def getLucky(self, s: str, k: int) -> int:
+        alphabet = {'a':1,'b':2,'c':3,'d':4,'e':5,'f':6,'g':7,'h':8,'i':9,'j':10,'k':11,
+        'l':12,'m':13,'n':14,'o':15,'p':16,'q':17,'r':18,'s':19,'t':20,'u':21,'v':22,'w':23,'x':24,'y':25,'z':26}
+        string_number_code = ""
+        for i in range(len(s)):
+            string_number_code+=str(alphabet[s[i]])
+        digit_sum = 0
+        print(string_number_code)
+        while(k>0):
+            while(string_number_code!='0'):
+                digit_sum += int(string_number_code)%10
+                string_number_code = str(int(string_number_code)//10)
+            k-=1
+            if k != 0: 
+                string_number_code = str(digit_sum)
+                digit_sum = 0
+        
+        return digit_sum
