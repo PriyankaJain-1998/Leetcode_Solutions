cacf0e5a5d9fb919c1cd7be14110b86df6fe4201
PriyankaJain-1998 <58905207+PriyankaJain-1998@users.noreply.github.com>
2024-09-07 20:24:42 -0400

    Time: 556 ms (85.6%), Space: 0B (100%) - LeetHub

--- 1882-the-number-of-employees-which-report-to-each-employee/1882-the-number-of-employees-which-report-to-each-employee.sql
+++ 1882-the-number-of-employees-which-report-to-each-employee/1882-the-number-of-employees-which-report-to-each-employee.sql
@@ -1,6 +1,21 @@
 # Write your MySQL query statement below
-select mgr.employee_id, 
-mgr.name, count(emp.employee_id) as reports_count, round(avg(emp.age)) as average_age
-from Employees emp join employees mgr on emp.reports_to = mgr.employee_id
-group by employee_id
-order by employee_id
+-- select mgr.employee_id, 
+-- mgr.name, count(emp.employee_id) as reports_count, round(avg(emp.age)) as average_age
+-- from Employees emp join employees mgr on emp.reports_to = mgr.employee_id
+-- group by employee_id
+-- order by employee_id
+
+SELECT
+    m.employee_id AS employee_id,
+    m.name AS name,
+    COUNT(e.employee_id) AS reports_count,
+    ROUND(AVG(e.age)) AS average_age
+FROM
+    employees m
+    JOIN employees e ON m.employee_id = e.reports_to
+GROUP BY
+    m.employee_id, m.name
+HAVING
+    COUNT(e.employee_id) > 0
+ORDER BY
+    m.employee_id;
