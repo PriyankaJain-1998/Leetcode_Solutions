ae5a90e552a9e61e16ce5adacd4762ef2806de3f
PriyankaJain-1998 <58905207+PriyankaJain-1998@users.noreply.github.com>
2024-09-17 18:16:21 -0400

    Time: 38 ms (71.4%), Space: 16.7 MB (34.76%) - LeetHub

---
+++ 1524-string-matching-in-an-array/1524-string-matching-in-an-array.py
@@ -0,0 +1,7 @@
+class Solution:
+    def stringMatching(self, words: List[str]) -> List[str]:
+        arr = ' '.join(words)
+        subStr = [i for i in words if arr.count(i) >= 2]
+              
+        return subStr
+                
