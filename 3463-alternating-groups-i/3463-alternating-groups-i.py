09b3f72cc5556cdefe2d31586302338f97d618eb
PriyankaJain-1998 <58905207+PriyankaJain-1998@users.noreply.github.com>
2024-09-21 21:20:37 -0400

    Time: 64 ms (6.77%), Space: 16.7 MB (38.86%) - LeetHub

--- 3463-alternating-groups-i/3463-alternating-groups-i.py
+++ 3463-alternating-groups-i/3463-alternating-groups-i.py
@@ -5,12 +5,6 @@ class Solution:
         colors.append(colors[1])
         window_size, count = 3, 0
         for i in range(len(colors)):
-            # check = colors[i:i+window_size]
-            # if window_size-len(check)==1: check.append(colors[0])
-            # elif window_size-len(check)==2: 
-            #     check.append(colors[0])
-            #     check.append(colors[1])
-
             if ''.join(map(str,colors[i:i+window_size])) in a:
                 count+=1
 
