3a88e7baad853b320c15e889fcecb655d20ac7fc
PriyankaJain-1998 <58905207+PriyankaJain-1998@users.noreply.github.com>
2024-09-21 23:41:46 -0400

    Time: 103 ms (64.11%), Space: 16.7 MB (7.68%) - LeetHub

--- 3193-maximum-strong-pair-xor-i/3193-maximum-strong-pair-xor-i.py
+++ 3193-maximum-strong-pair-xor-i/3193-maximum-strong-pair-xor-i.py
@@ -1,13 +1,9 @@
 class Solution:
     def maximumStrongPairXor(self, nums: List[int]) -> int:
-        # strong_pairs = [[x, x] for x in nums]
-        
-        # strong_pairs = [(x,y) for x in nums for y in nums if x<=y and abs(x-y)<=min(x,y)]
-        # print(strong_pairs, len(strong_pairs))
-        xor_output = 0
-        for x in nums:
-            for y in nums: 
-                if x<=y and abs(x-y)<=min(x,y):
-                    xor_output = max(xor_output, x^y)
+        # xor_output = 0
+        # for x in nums:
+        #     for y in nums: 
+        #         if x<=y and abs(x-y)<=min(x,y):
+        #             xor_output = max(xor_output, x^y)
 
-        return xor_output
+        return max(x^y for x in nums for y in nums if x<=y and abs(x-y)<=min(x,y))
