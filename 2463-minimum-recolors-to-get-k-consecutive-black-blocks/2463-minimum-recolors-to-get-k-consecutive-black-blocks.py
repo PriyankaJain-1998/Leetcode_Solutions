592b2006fe9713dda8d6c1eea1e456f0b60cede6
PriyankaJain-1998 <58905207+PriyankaJain-1998@users.noreply.github.com>
2024-09-21 23:58:50 -0400

    Time: 34 ms (72.29%), Space: 16.5 MB (80.11%) - LeetHub

--- 2463-minimum-recolors-to-get-k-consecutive-black-blocks/2463-minimum-recolors-to-get-k-consecutive-black-blocks.py
+++ 2463-minimum-recolors-to-get-k-consecutive-black-blocks/2463-minimum-recolors-to-get-k-consecutive-black-blocks.py
@@ -4,7 +4,6 @@ class Solution:
         for i in range(len(blocks)):
             sub_str = blocks[i:i+k]
             num_oper = sub_str.count('W')
-            print(blocks[i:i+k], blocks[i:i+k].count("W"))
             if len(sub_str)!=k:break
             if num_oper == 0: return 0
             else: output = min(output, num_oper)
