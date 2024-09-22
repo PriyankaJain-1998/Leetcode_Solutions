ff509bffe65f94040580f6060f96c545486ee79c
PriyankaJain-1998 <58905207+PriyankaJain-1998@users.noreply.github.com>
2024-09-21 21:20:10 -0400

    Time: 62 ms (6.77%), Space: 16.6 MB (38.86%) - LeetHub

--- 3463-alternating-groups-i/3463-alternating-groups-i.py
+++ 3463-alternating-groups-i/3463-alternating-groups-i.py
@@ -1,14 +1,15 @@
 class Solution:
     def numberOfAlternatingGroups(self, colors: List[int]) -> int:
         a = ['101', '010']
-        # colors.append(colors[0])
+        colors.append(colors[0])
+        colors.append(colors[1])
         window_size, count = 3, 0
         for i in range(len(colors)):
             check = colors[i:i+window_size]
-            if window_size-len(check)==1: check.append(colors[0])
-            elif window_size-len(check)==2: 
-                check.append(colors[0])
-                check.append(colors[1])
+            # if window_size-len(check)==1: check.append(colors[0])
+            # elif window_size-len(check)==2: 
+            #     check.append(colors[0])
+            #     check.append(colors[1])
 
             if ''.join(map(str,check)) in a:
                 count+=1
