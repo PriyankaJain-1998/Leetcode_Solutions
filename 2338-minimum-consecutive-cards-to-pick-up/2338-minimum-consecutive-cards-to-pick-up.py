3f5d1653b9959ff9a49901597deeb74f5902bd7f
PriyankaJain-1998 <58905207+PriyankaJain-1998@users.noreply.github.com>
2024-10-05 14:54:19 -0400

    Time: 613 ms (61.48%), Space: 35.9 MB (49.97%) - LeetHub

--- 2338-minimum-consecutive-cards-to-pick-up/2338-minimum-consecutive-cards-to-pick-up.py
+++ 2338-minimum-consecutive-cards-to-pick-up/2338-minimum-consecutive-cards-to-pick-up.py
@@ -1,8 +1,8 @@
 class Solution:
     def minimumCardPickup(self, cards: List[int]) -> int:
-        res, pos = 10**6+1, {}
+        res, pos = 10**5+1, {}
         for i,c in enumerate(cards):
             if c in pos:
                 res = min(res, i-pos[c]+1)
             pos[c]=i
-        return res%(10**6+1) or -1 
+        return res%(10**5+1) or -1 
