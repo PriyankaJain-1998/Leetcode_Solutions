9e91b9fabaded354b4e1ddd99668ba98e169a34a
PriyankaJain-1998 <58905207+PriyankaJain-1998@users.noreply.github.com>
2024-10-10 19:48:28 -0400

    Time: 882 ms (42.49%), Space: 22.4 MB (19.85%) - LeetHub

--- 1631-number-of-sub-arrays-with-odd-sum/1631-number-of-sub-arrays-with-odd-sum.py
+++ 1631-number-of-sub-arrays-with-odd-sum/1631-number-of-sub-arrays-with-odd-sum.py
@@ -1,7 +1,7 @@
 class Solution:
     def numOfSubarrays(self, arr: List[int]) -> int:
         dp = [0]*len(arr)
-        dp[0] = arr[0]&1
+        dp[0] = arr[0]%2
         for i in range(1,len(arr)):
             if arr[i]%2!=0: dp[i]=i-dp[i-1]+1
             else: dp[i] = dp[i-1]
