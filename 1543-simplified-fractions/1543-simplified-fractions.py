a85af45af45012dfb8794d7fd39a9f0e1e834ef3
PriyankaJain-1998 <58905207+PriyankaJain-1998@users.noreply.github.com>
2024-10-10 21:44:31 -0400

    Time: 674 ms (5.12%), Space: 18 MB (8.05%) - LeetHub

---
+++ 1543-simplified-fractions/1543-simplified-fractions.py
@@ -0,0 +1,23 @@
+class Solution:
+    def simplifiedFractions(self, n: int) -> List[str]:
+        fractions, decimals = [], []
+        numerator = 1
+        while numerator<n:
+            denominator = 2
+            while denominator<=n:
+                fraction = numerator/denominator
+                if fraction < 1:
+                    if fraction not in decimals:
+                        decimals.append(fraction)
+                        fractions.append(str(numerator)+"/"+str(denominator))
+                denominator+=1
+            numerator+=1
+            print(denominator)
+        print(fractions)
+
+        return fractions
+
+
+
+
+        
