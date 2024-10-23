3a5aeeffd11171bbb43a79cceea3dcf54e7dfaad
PriyankaJain-1998 <58905207+PriyankaJain-1998@users.noreply.github.com>
2024-10-23 15:06:43 -0400

    Time: 0 ms (100%), Space: 16.5 MB (23.82%) - LeetHub

---
+++ 2427-first-letter-to-appear-twice/2427-first-letter-to-appear-twice.py
@@ -0,0 +1,11 @@
+class Solution:
+    def repeatedCharacter(self, s: str) -> str:
+        
+        setS = set()
+
+        for x in s:
+            if x in setS:
+                return x
+            else:
+                setS.add(x)
+        
