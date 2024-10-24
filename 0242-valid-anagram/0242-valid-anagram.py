ec1bb32b969779ee46ca6255f80c10ed2125730b
PriyankaJain-1998 <58905207+PriyankaJain-1998@users.noreply.github.com>
2024-10-23 23:31:30 -0400

    Time: 3115 ms (5.05%), Space: 16.6 MB (98.07%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if set(s)==set(t) and len(s)==len(t): 
+            for i in s:
+                if s.count(i)!=t.count(i): return False
+            
+            return True
+        return False
