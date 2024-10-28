a6c42fbeca9349b9d849e1341cee58da7883ada7
PriyankaJain-1998 <58905207+PriyankaJain-1998@users.noreply.github.com>
2024-10-27 21:39:50 -0400

    Time: 0 ms (100%), Space: 16.6 MB (61.26%) - LeetHub

--- 1626-can-make-arithmetic-progression-from-sequence/1626-can-make-arithmetic-progression-from-sequence.py
+++ 1626-can-make-arithmetic-progression-from-sequence/1626-can-make-arithmetic-progression-from-sequence.py
@@ -3,6 +3,5 @@ class Solution:
         arr = sorted(arr)
         diff = abs(arr[0]-arr[1])
         for i in range(2,len(arr)):
-            # print(arr, diff,arr[i]-arr[i-1] )
             if abs(arr[i]-arr[i-1])!=diff: return False
         return True
