bcb50ae2a19ca64f2c177a58383a820a7de37126
PriyankaJain-1998 <58905207+PriyankaJain-1998@users.noreply.github.com>
2024-11-12 19:04:13 -0500

    Time: 4 ms (10.92%), Space: 16.8 MB (15.28%) - LeetHub

--- 3447-clear-digits/3447-clear-digits.py
+++ 3447-clear-digits/3447-clear-digits.py
@@ -1,18 +1,7 @@
 class Solution:
     def clearDigits(self, s: str) -> str:
-        # s = list(s)
-        # for i in range(len(s)):
-        #     if s[i].isdigit():
-        #         del s[i]
-        #         del s[i-1]
-        #         print(s)
         stack = []  
         for i in s: 
-            if i.isdigit():
-                stack.pop()
-                # index = s.index(i)
-                # s = s.replace(i,"").replace(s[index-1],"",1)
-                # print(index, s)
+            if i.isdigit():stack.pop()
             else: stack.append(i)
-        print(stack)
         return "".join(stack)
