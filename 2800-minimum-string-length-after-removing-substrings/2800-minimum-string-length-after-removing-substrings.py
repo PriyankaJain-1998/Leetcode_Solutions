8d3708afb9c130e9e23eb5c13c485fc759001e4f
PriyankaJain-1998 <58905207+PriyankaJain-1998@users.noreply.github.com>
2024-11-12 19:14:01 -0500

    Time: 7 ms (56.97%), Space: 16.6 MB (59.64%) - LeetHub

--- 2800-minimum-string-length-after-removing-substrings/2800-minimum-string-length-after-removing-substrings.py
+++ 2800-minimum-string-length-after-removing-substrings/2800-minimum-string-length-after-removing-substrings.py
@@ -6,7 +6,6 @@ class Solution:
             for i in s: 
                 stack.append(i)
                 if len(stack)>1:
-                    print(stack, stack[-1], stack[-2])
                     if stack[-2]+stack[-1] in string: 
                         stack.pop()
                         stack.pop()
