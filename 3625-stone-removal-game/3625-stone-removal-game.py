d0c7e5f773aab6ad3f7de6284b5b1a8f0038bf49
PriyankaJain-1998 <58905207+PriyankaJain-1998@users.noreply.github.com>
2024-11-23 20:29:41 -0500

    Time: 0 ms (100%), Space: 16.6 MB (100%) - LeetHub

---
+++ 3625-stone-removal-game/3625-stone-removal-game.py
@@ -0,0 +1,9 @@
+class Solution:
+    def canAliceWin(self, n: int) -> bool:
+        if n<10: return False
+        count, pile = 0, 10 
+        while n>=pile:
+            n-=pile
+            pile-=1
+            count +=1
+        return count%2!=0
