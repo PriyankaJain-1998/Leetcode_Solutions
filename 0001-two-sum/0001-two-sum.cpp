b02db25f9dc2fe913c8515c56bf6670b06f31f33
PriyankaJain-1998 <58905207+PriyankaJain-1998@users.noreply.github.com>
2025-08-04 19:24:17 -0400

    Time: 0 ms (100%), Space: 15 MB (13.77%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,13 +1,31 @@
+// --- BRUTE FORCE O(n^2)
+// class Solution {
+// public:
+//     vector<int> twoSum(vector<int>& nums, int target) {
+//         int n = nums.size();
+//         for (int i=0;i<n-1;i++){
+//             for (int j=i+1;j<n;j++){
+//                 if (nums[i]+nums[j]== target){
+//                     return {i,j};
+//                 }
+//             }
+//         }
+//         return {};
+//     }
+// };
+
+// better approach O(n)
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         int n = nums.size();
-        for (int i=0;i<n-1;i++){
-            for (int j=i+1;j<n;j++){
-                if (nums[i]+nums[j]== target){
-                    return {i,j};
-                }
+        map<int, int> mp;
+        for (int i=0;i<n;i++){
+            int remainder = target - nums[i];
+            if (mp.find(remainder) !=mp.end()){
+                return {mp[remainder],i};
             }
+            mp[nums[i]] = i;
         }
         return {};
     }
