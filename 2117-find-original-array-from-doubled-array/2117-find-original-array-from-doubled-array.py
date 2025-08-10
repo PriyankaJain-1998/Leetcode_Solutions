870326db3c4673e9f10bf92bf21b035e5662b791
PriyankaJain-1998 <58905207+PriyankaJain-1998@users.noreply.github.com>
2025-08-10 09:20:20 -0400

    Time: 448 ms (5.01%), Space: 35.4 MB (30.06%) - LeetHub

--- 2117-find-original-array-from-doubled-array/2117-find-original-array-from-doubled-array.py
+++ 2117-find-original-array-from-doubled-array/2117-find-original-array-from-doubled-array.py
@@ -8,16 +8,31 @@ class Solution:
         # elif list(map(lambda x: x * 2, right)) == left: return right
         # else: return []
 
-        if len(changed)%2 !=0: return []
+        # if len(changed)%2 !=0: return []
 
+        # changed = sorted(changed)
+        # c = Counter(changed)
+        # output = []
+        # for num in changed:
+        #     if num in c and c[num]>=1:
+        #         c[num]-=1
+        #         if (num*2) in c and c[(num*2)]>=1:
+        #             output.append(num)
+        #             c[num*2]-=1
+        #     if len(output)==len(changed)//2: return output 
+        # return []
+
+        if len(changed)%2!=0: return []
         changed = sorted(changed)
-        c = Counter(changed)
+        mp = Counter(changed)
         output = []
-        for num in changed:
-            if num in c and c[num]>=1:
-                c[num]-=1
-                if (num*2) in c and c[(num*2)]>=1:
-                    output.append(num)
-                    c[num*2]-=1
-            if len(output)==len(changed)//2: return output 
+        for i in changed:
+            twice = i*2
+            if mp[i] == 0: continue
+            if i in mp and mp[i]>0:
+                mp[i]-=1
+                if twice in mp and mp[twice]>0:
+                    output.append(i)
+                    mp[twice]-=1
+            if len(output)==len(changed)//2:return output
         return []
