fdfc1b93c06bdf3c2f0dcc9e3ad065a1ed481223
PriyankaJain-1998 <58905207+PriyankaJain-1998@users.noreply.github.com>
2025-08-10 08:20:07 -0400

    Time: 0 ms (100%), Space: 17.7 MB (53.52%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -15,9 +15,37 @@ class Solution:
         #         i+=1
         #         j-=1
 
-        for i in range(0,len(nums)):
-            for j in range(i+1, len(nums)):
-                if nums[i]>nums[j]:
-                    nums[i],nums[j]=nums[j],nums[i]
+        ### -- O(n^2)
+        # for i in range(0,len(nums)):
+        #     for j in range(i+1, len(nums)):
+        #         if nums[i]>nums[j]:
+        #             nums[i],nums[j]=nums[j],nums[i]
 
         
+        ### -- TLE
+        # n = len(nums)
+        # i,j,k = 0,0,n-1
+        # while(j<=k):
+        #     if (nums[j]==1): j+=1
+        #     elif nums[j]==2: nums[j], nums[k] = nums[k], nums[j]
+        #     else:
+        #         nums[j], nums[i] = nums[i], nums[j] 
+        #         i+=1
+        #         j+=1
+
+        ### 
+        i,j,k = 0,0,0
+        for n in nums:
+            if n==0: i+=1
+            elif n==1: j+=1
+            else: k+=1
+        for p in range(len(nums)):
+            if i>0:
+                nums[p]=0
+                i-=1
+            elif j>0:
+                nums[p]=1
+                j-=1
+            else:
+                nums[p]=2
+                k-=1
